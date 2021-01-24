cf6f9bcf4706f285ed1983cd250cfd032f4bba03
ranjit <ranjitmaheshshah@gmail.com>
2021-01-25 04:45:54 +0530

    api integreted

--- src/comp/tableData/TableData.js
+++ src/comp/tableData/TableData.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { throttle } from 'lodash';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,45 +12,46 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
 const columns = [
-    { id: 'bank', label: 'BankId', minWidth: 170 },
-    { id: 'ifsc', label: 'IFSC Code', minWidth: 100 },
+    { id: 'bank', label: 'BankId',align: 'center', },
+    { id: 'ifsc', label: 'IFSC Code',align: 'center', },
     {
         id: 'branch',
         label: 'Branch',
         minWidth: 170,
-        align: 'right',
+        align: 'center',
     },
     {
         id: 'address',
         label: 'Address',
         minWidth: 170,
-        align: 'right',
+        align: 'center',
     },
     {
         id: 'city',
         label: 'City',
         minWidth: 170,
-        align: 'right',
+        align: 'center',
     },
     {
         id: 'district',
         label: 'district',
         minWidth: 170,
-        align: 'right',
+        align: 'center',
     },
     {
         id: 'state',
         label: 'state',
         minWidth: 170,
-        align: 'right',
+        align: 'center',
     },
 ];
 
-const currencies = [
+const cities = [
     {
         value: 'banglore',
         label: 'banglore',
@@ -59,44 +62,69 @@ const currencies = [
     },
     {
         value: 'Delhi',
-        label: 'ghazibad',
+        label: 'Delhi',
     },
 ];
 
-function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-}
+// function createData(name, code, population, size) {
+//     const density = population / size;
+//     return { name, code, population, size, density };
+// }
 
-const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-];
+// const rows = [
+//     createData('India', 'IN', 1324171354, 3287263),
+//     createData('China', 'CN', 1403500365, 9596961),
+//     createData('Italy', 'IT', 60483973, 301340),
+//     createData('United States', 'US', 327167434, 9833520),
+//     createData('Canada', 'CA', 37602103, 9984670),
+//     createData('Australia', 'AU', 25475400, 7692024),
+//     createData('Germany', 'DE', 83019200, 357578),
+//     createData('Ireland', 'IE', 4857000, 70273),
+//     createData('Mexico', 'MX', 126577691, 1972550),
+//     createData('Japan', 'JP', 126317000, 377973),
+//     createData('France', 'FR', 67022000, 640679),
+//     createData('United Kingdom', 'GB', 67545757, 242495),
+//     createData('Russia', 'RU', 146793744, 17098246),
+//     createData('Nigeria', 'NG', 200962417, 923768),
+//     createData('Brazil', 'BR', 210147125, 8515767),
+// ];
 
 
 
 export default function TableData() {
     const classes = useStyles();
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-    const [currency, setCurrency] = React.useState('EUR');
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [city, setCity] = useState('');
+    const [search, setsearch] = useState('')
+    const [rows, setrows] = useState([])
+
+    console.log(rows)
+
+    useEffect(() => {
+        getAllData()
+        throttled.current(search)
+    },[search])
+
+    const throttled = useRef(throttle((newValue) => console.log(newValue), 1000))
+
+    const getAllData = () => {
+        console.log(search)
+        axios.get(`http://127.0.0.1:8000/api/branches/?q=${search}`).then((res) => {
+            // console.log(res.data.results)
+            setrows(res.data.results)
+        })
+    }
+
+
 
     const handleChange = (event) => {
-        setCurrency(event.target.value);
+        setCity(event.target.value);
     };
+
+    const handleSearch = (e)=>{
+        setsearch(e.target.value)
+    }
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -115,24 +143,27 @@ export default function TableData() {
                     id="standard-select-currency"
                     select
                     label="Bank"
-                    value={currency}
+                    value={city}
                     onChange={handleChange}
                     variant="outlined"
                     helperText="Please select your bank city"
                 >
-                    {currencies.map((option) => (
+                    {cities.map((option) => (
                         <MenuItem key={option.value} value={option.value}>
                             {option.label}
                         </MenuItem>
                     ))}
                 </TextField>
-                <TextField id="standard-basic" label="Search" />
+                <TextField id="standard-basic" onChange={handleSearch} label="Search" />
 
             </div>
             <TableContainer className={classes.container}>
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
                         <TableRow>
+                            <TableCell align='center' >
+                                S.No.
+                            </TableCell>
                             {columns.map((column) => (
                                 <TableCell
                                     key={column.id}
@@ -145,9 +176,12 @@ export default function TableData() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                      {rows?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row,index) => {
                             return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                <TableRow hover role="checkbox" tabIndex={-1} key={index+1}>
+                                    <TableCell key={index+1} align='center' >
+                                        {index+1}
+                                    </TableCell>
                                     {columns.map((column) => {
                                         const value = row[column.id];
                                         return (
@@ -163,7 +197,7 @@ export default function TableData() {
                 </Table>
             </TableContainer>
             <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
+                rowsPerPageOptions={[10, 25, 50, 100]}
                 component="div"
                 count={rows.length}
                 rowsPerPage={rowsPerPage}
